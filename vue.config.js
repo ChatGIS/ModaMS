2cf9ea5e14c4a4cb6c32759d4990a070f61df439
Dreamice <dreamice13@foxmail.com>
2022-11-14 19:09:08 +0800

    添加publicPath配置项

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ const Components = require('unplugin-vue-components/webpack')
 const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 
 module.exports = {
+    publicPath: '/mdms/',
     configureWebpack: config => {
         config.plugins.push(AutoImport({
             resolvers: [ElementPlusResolver()],
