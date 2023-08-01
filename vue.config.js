de885830d5d8e8caa07465b810c6d7c1b87c6ad6
Dreamice <dreamice13@foxmail.com>
2023-08-01 23:37:13 +0800

    修改项目名

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,17 @@
+/*
+ * @Author: Dreamice dreamice13@foxmail.com
+ * @Date: 2022-11-13 17:44:04
+ * @LastEditors: Dreamice dreamice13@foxmail.com
+ * @LastEditTime: 2023-08-01 11:21:13
+ * @FilePath: \ModaMS\vue.config.js
+ * @Description: 
+ */
 const AutoImport = require('unplugin-auto-import/webpack')
 const Components = require('unplugin-vue-components/webpack')
 const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 
 module.exports = {
-    publicPath: '/mdms/',
+    publicPath: '/moda/',
     configureWebpack: config => {
         config.plugins.push(AutoImport({
             resolvers: [ElementPlusResolver()],
